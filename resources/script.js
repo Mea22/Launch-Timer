c638584caa64d76a05cb285b860075b40d161b53
Mea22 <113638917+Mea22@users.noreply.github.com>
2023-07-30 18:30:51 +0300

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -6,7 +6,7 @@ const  s = document.getElementById("seconds")
 //get set interval and write function that gets days hours minutes and seconds from miliseconds and puts it on html.
 const set = setInterval( (() =>{
 
-    let start = new Date("may 25").getTime();
+    let start = new Date("may 25,2024 00:01").getTime();
     let now = new Date().getTime();
     let time = start - now;
 
